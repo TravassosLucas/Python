ae6a72c3e35c3cc872c2c34ec123af396d3ee6c7
Lucas Cruz <travassoscruzlucas@gmail.com>
2023-09-03 16:06:41 -0300

    aula-py

---
+++ exec-py/aula2.py
@@ -0,0 +1,5 @@
+dia = input ('Dia que você nasceu?')
+mes = input ('Mês que tu nasceu')
+ano = input ('Que ano você nasceu?')
+
+print ('Você nasceu no dia' , dia , 'de' , mes , 'de' , ano , '. Correto?') 
---
+++ exec-py/aula3.py
@@ -0,0 +1,4 @@
+numero1 = input ('Digite um número')
+numero2 = input ('Digite o outro número')
+
+print (numero1  , '+' , numero2)
