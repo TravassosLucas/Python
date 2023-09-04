f7ff29db0de6d18d4a57fd04a0b6dea15ca118f0
Lucas Cruz <travassoscruzlucas@gmail.com>
2023-09-04 19:47:06 -0300

    04/09/23

---
+++ módulo1/aula5 atencessor-sucessor.py
@@ -0,0 +1,4 @@
+n = int (input('Digite o número escolhido por ti'))
+
+print ('O seu antecessor é {} e seu sucessor é {}' .format ( n-1 , n+1 ))
+
---
+++ módulo1/aula6. dobro-triplo-raiz.py
@@ -0,0 +1,4 @@
+n = int (input('Digite seu número'))
+
+
+print ('O dobro de seu número é {} , o triplo de seu número é {} , a raiz quadrada de seu número é {}' .format( n*2 , n*3 , n**(1/2) ))
