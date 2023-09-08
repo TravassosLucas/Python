7ed53af8e568cf40eb2526cd454b8552f71c58c7
Lucas Cruz <travassoscruzlucas@gmail.com>
2023-09-08 15:41:19 -0300

    feito

---
+++ módulo1/aula 19 sorteio.py
@@ -0,0 +1,6 @@
+import random
+
+nomes = ('Lucas' , 'Bruna', 'Gustavo', 'Pedro')
+nome = random.choice(nomes)
+
+print(f'{nome}')
--- módulo1/teste import2.py
+++ módulo1/teste import2.py
@@ -1,3 +1,5 @@
 import emoji
-print(emoji.emojize('Olá mundo :sunglasses:' , use_aliases=True))
 
+print(emoji.emojize('Olá mundo \U0001F60E '))
+
+#no caso nosso não é possível utilizar o :aliase pois a biblioteca não suporta ou seja precisamos utilizar o código dela
